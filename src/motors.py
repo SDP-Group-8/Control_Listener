0770cd833e253163d48308a57346b5bcc6d66cfa
AlexWebb03 <alexanderwebb03@gmail.com>
2024-02-27 15:42:00 +0000

    update

--- src/motors.py
+++ src/motors.py
@@ -1,7 +1,9 @@
 import time
 import RPi.GPIO as IO
+import rospy
+from std_msgs.msg import Float32
 
-class cameraMount:
+class ControlListenerNode:
     def __init__(self):
         self.YELLOW1 = 26
         self.BLUE1 = 19
@@ -27,8 +29,8 @@ class cameraMount:
         IO.setup(self.YELLOW2, IO.IN, pull_up_down=IO.PUD_DOWN)
         IO.setup(self.BLUE2, IO.IN, pull_up_down=IO.PUD_DOWN)
 
-        IO.add_event_detect(self.YELLOW1, IO.RISING, callback=self.motor1Callback, bouncetime=30)
-        IO.add_event_detect(self.YELLOW2, IO.RISING, callback=self.motor2Callback, bouncetime=30)
+        IO.add_event_detect(self.YELLOW1, IO.RISING, callback=self.motor_1_callback, bouncetime=30)
+        IO.add_event_detect(self.YELLOW2, IO.RISING, callback=self.motor_1_callback, bouncetime=30)
 
         IO.setup(self.M1_1, IO.OUT)
         IO.setup(self.M1_2, IO.OUT)
@@ -41,7 +43,14 @@ class cameraMount:
         self.motor2_1 = IO.PWM(self.M2_1, 100)
         self.motor2_2 = IO.PWM(self.M2_2, 100)
 
-    def motor1Callback(self, channel):
+    def set_distance_callback(self, dst):
+        self.move_motors(dst)
+
+    def init(self):
+        rospy.Subscriber("distance", Float32, self.set_distance_callback)
+        rospy.init_node("control_listener_node", anonymous=True)
+
+    def motor_1_callback(self, channel):
         blue = IO.input(self.BLUE1)
         yellow = IO.input(self.YELLOW1)
         if blue == yellow:
@@ -52,9 +61,8 @@ class cameraMount:
             self.motor1_1.stop()
             self.motor1_2.stop()
             self.targetPos1 = None
-        # print("M1:", self.degrees1)
 
-    def motor2Callback(self, channel):
+    def motor_1_callback(self, channel):
         blue = IO.input(self.BLUE2)
         yellow = IO.input(self.YELLOW2)
         if blue == yellow:
@@ -65,10 +73,8 @@ class cameraMount:
             self.motor2_1.stop()
             self.motor2_2.stop()
             self.targetPos2 = None
-        # print("M2:", self.degrees2)
 
-
-    def moveMotors(self, degrees):
+    def move_motors(self, degrees):
         targetPos = self.degrees1 + degrees
         if 0 <= targetPos <= 134:
             self.targetPos1 = targetPos
@@ -95,3 +101,7 @@ class cameraMount:
             print("Done Moving")
         else: 
             print("Degrees out of range or invalid")
+
+if __name__ == '__main__':
+    node = ControlListenerNode()
+    node.init()
