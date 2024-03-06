3b4b78ac5f7f869b0d720e73d2ce6b4b56247c6e
Tom Wood <thomaswood100@gmail.com>
2024-03-06 15:57:38 +0000

    increase pwm speed

--- motors.py
+++ motors.py
@@ -51,8 +51,8 @@ class cameraMount:
         IO.setup(self.M2_1, IO.OUT)
         IO.setup(self.M2_2, IO.OUT)
 
-        self.motor1 = IO.PWM(self.ENA, 100)
-        self.motor2 = IO.PWM(self.ENB, 100)
+        self.motor1 = IO.PWM(self.ENA, 500)
+        self.motor2 = IO.PWM(self.ENB, 500)
 
         self.motor1.start(0)
         self.motor2.start(0)
