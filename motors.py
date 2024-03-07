849247d6efe66b0acc9f9736d3351524a9628b17
jan <janokul92@gmail.com>
2024-03-07 14:57:03 +0000

    c5

--- motors.py
+++ motors.py
@@ -119,10 +119,10 @@ class cameraMount:
         if self.minPos <= position <= self.maxPos:
             self.targetPos = position
             self.pid_controller.set_endpoint = position
-            # self.setMotor1Speed(self.motorSpeed)
-            self.setMotor2Speed(self.motorSpeed)
-            # self.setMotor1Direction("up")
-            self.setMotor2Direction("up")
+            self.setMotor1Speed(self.motorSpeed)
+            # self.setMotor2Speed(self.motorSpeed)
+            self.setMotor1Direction("up")
+            # self.setMotor2Direction("up")
 
         else:
             print("Invalid Position Input")
