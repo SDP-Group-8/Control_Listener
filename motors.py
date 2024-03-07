31722d4f81b81fba2371cc4d1bb60ebda1aada6e
jan <janokul92@gmail.com>
2024-03-07 15:37:47 +0000

    c5

--- motors.py
+++ motors.py
@@ -165,7 +165,7 @@ class cameraMount:
 if __name__ == '__main__':
     try:
         c = cameraMount(185)
-        c.setCameraHeight(165)
+        c.setCameraHeight(160)
         time.sleep(20)
         c.motor1.stop()
         c.motor2.stop()
